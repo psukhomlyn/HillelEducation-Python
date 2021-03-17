6e19ed5db02051f9dbc0f87d8a15706b036e864e
psukhomlyn <80566669+psukhomlyn@users.noreply.github.com>
2021-03-17 17:18:29 +0200

    Add files via upload

---
+++ Lesson2/HW-3-calculator.py
@@ -0,0 +1,27 @@
+import sys
+
+first_value: str = input('Input first value: ')
+operation: str = input('Input math operation: ')
+second_value: str = input('Input second value: ')
+
+for value in {first_value, second_value}:
+    if not value.isdigit():
+        print(f'Value of type {type(value)} {value} is not a digit')
+        sys.exit(0)
+    first_value: int = int(first_value)
+    second_value: int = int(second_value)
+
+if operation == '+':
+    print(first_value + second_value)
+elif operation == '-':
+    print(first_value - second_value)
+elif operation == '*':
+    print(first_value * second_value)
+elif operation == '/':
+    print(first_value / second_value)
+elif operation == '//':
+    print(first_value // second_value)
+elif operation == 'sqr':
+    print(first_value * first_value)
+else:
+    print(f'Invalid operation: {operation}')
