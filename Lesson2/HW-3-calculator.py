146899b2b29140a8a1ee2d775fb2f0de525a40e9
psukhomlyn <petrsukhomlin@gmail.com>
2021-03-17 17:43:25 +0200

    1 commit

---
+++ Lesson2/HW-3-calculator.py
@@ -0,0 +1,29 @@
+import sys
+
+first_value: str = input('Input first value: ')
+operation: str = input('Input math operation: ')
+second_value: str = input('Input second value: ')
+
+for value in {first_value, second_value}:
+    if not value.isdigit():
+        print(f'Value of type {type(value)} {value} is not a digit')
+        sys.exit(0)
+    first_value: int = int(first_value)
+    second_value: int = int(second_value)
+
+if operation == '/' and second_value == 0:
+    print('Division by 0 not possible')
+elif operation == '/':
+    print(first_value / second_value)
+elif operation == '+':
+    print(first_value + second_value)
+elif operation == '-':
+    print(first_value - second_value)
+elif operation == '*':
+    print(first_value * second_value)
+elif operation == '//':
+    print(first_value // second_value)
+elif operation == 'sqr':
+    print(first_value * first_value)
+else:
+    print(f'Invalid operation: {operation}')
