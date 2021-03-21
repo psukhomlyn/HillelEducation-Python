b4bde21b8628ae7fe9f76b7caf4dd27a21d6fc12
psukhomlyn <petrsukhomlin@gmail.com>
2021-03-21 18:39:54 +0200

    commit HT

--- Lesson3/HW-3-1.py
+++ Lesson3/HW-3-1.py
@@ -5,28 +5,30 @@ second_value: str = input('Input second value: ')
 try:
     first_value: int = int(first_value)
     second_value: int = int(second_value)
-except IndentationError:
-    print (f'Value of type {type(value)} {value} is not a digit')
-try:
-    result = first_value / second_value
-except ZeroDivisionError:
-    print('Division by 0 not possible')
-
-
-# if operation == '/':
-#     if second_value == 0:
-#         print('Division by 0 not possible')
-#     elif second_value!=0:
-#         print(first_value / second_value)
-# elif operation == '+':
-#     print(first_value + second_value)
-# elif operation == '-':
-#     print(first_value - second_value)
-# elif operation == '*':
-#     print(first_value * second_value)
-# elif operation == '//':
-#     print(first_value // second_value)
-# elif operation == 'sqr':
-#     print(first_value * first_value)
-# else:
-#     print(f'Invalid operation: {operation}')
+except ValueError:
+    print (f'Both values should be a digits')
+else:
+    if operation == '+':
+        print(first_value + second_value)
+    elif operation == '-':
+        print(first_value - second_value)
+    elif operation == '*':
+        print(first_value * second_value)
+    elif operation == '/':
+        try:
+            result1 = first_value / second_value
+        except ZeroDivisionError:
+            print('Division by 0 not possible')
+        else:
+            print(result1)
+    elif operation == '//':
+        try:
+            result2 = first_value // second_value
+        except ZeroDivisionError:
+            print('Division by 0 not possible')
+        else:
+            print(result2)
+    elif operation == '**':
+        print(first_value * first_value)
+    else:
+        print(f'Invalid operation: {operation}')
