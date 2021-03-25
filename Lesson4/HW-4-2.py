52515215cd44b0b5c421ac21d3fb4c6f92f52ab6
psukhomlyn <petrsukhomlin@gmail.com>
2021-03-25 11:27:02 +0200

    commit HT

--- Lesson4/HW-4-2.py
+++ Lesson4/HW-4-2.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 date = 'Feb 12 2019 2:41PM'
-
-print(datetime.strptime(date, '%b %d %Y %I:%M%p'))
+converted_time = datetime.strptime(date, '%b %d %Y %I:%M%p')
+print(f'Converted time is: {converted_time}')
