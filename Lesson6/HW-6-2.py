51b2422df01bdc17952d6a575f7665838f719c39
psukhomlyn <petrsukhomlin@gmail.com>
2021-03-31 16:34:20 +0300

    commit HT

--- Lesson6/HW-6-2.py
+++ Lesson6/HW-6-2.py
@@ -1,16 +1,13 @@
 number: str = input('Please enter a number: ')
 
 if not number.isdigit():
-    raise ValueError('Entered value should be a number')
+    raise ValueError('Entered value should be an integer')
 
 number: int = int(number)
 
 
-def even_odd_number():
-    if number % 2 == 0:
+def even_odd_number(num):
+    if num % 2 == 0:
         return True
     else:
         return False
-
-
-print(even_odd_number())
