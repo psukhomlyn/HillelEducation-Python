13e2bd7a84007531188afaa020f8a9c5fc6178a2
psukhomlyn <petrsukhomlin@gmail.com>
2021-04-07 16:22:13 +0300

    commit HT

--- Lesson8/HW-8-2.py
+++ Lesson8/HW-8-2.py
@@ -5,24 +5,32 @@
 """
 import random
 
+first_player_name = input('Enter 1st player name: ')
+second_player_name = input('Enter 2nd player name: ')
+
 
 def first_player():
-    input('Игрок 1, ваш ход')
+    input(f'{first_player_name} move ')
     return random.randint(1, 6)
 
 
 def second_player():
-    input('Игрок 2, ваш ход')
+    input(f'{second_player_name} move')
     return random.randint(1, 6)
 
 
 def main():
-    start = input('Start game yes|no ')
+    start = input('Start game yes|no: ')
     player_1_sum = 0
     player_2_sum = 0
     if start == 'yes':
-        counter = 10
+        counter = input('How many rounds game do you want play? ')
+        if not counter.isdigit():
+            raise TypeError('Number of rounds should be a digit')
+        counter = int(counter)
+        round_num = 1
         while counter:
+            print(f'{round_num} round')
             player_1_value = first_player()
             player_2_value = second_player()
             if player_1_value == player_2_value:
@@ -30,25 +38,27 @@ def main():
             player_1_sum += player_1_value
             player_2_sum += player_2_value
 
-            print(f'First player result in {11-counter} round is {player_1_value}')
-            print(f'Second player result in {11 - counter} round is {player_2_value}')
+            print(f'{first_player_name} result in {round_num} round is {player_1_value}')
+            print(f'{second_player_name} result in {round_num} round is {player_2_value}')
 
             counter -= 1
+            round_num += 1
 
         if player_1_sum > player_2_sum:
-            print('First player win')
-            print(player_1_sum)
-            print(player_2_sum)
+            print(f'{first_player_name} win with result {player_1_sum} : {player_2_sum}')
+            # print(player_1_sum)
+            # print(player_2_sum)
             return
 
         if player_1_sum == player_2_sum:
-            print('Draw')
-            print(player_1_sum)
-            print(player_2_sum)
+            print(f'Draw with result {player_1_sum} : {player_2_sum}')
+            # print(player_1_sum)
+            # print(player_2_sum)
             return
 
-        print('Second player win')
-        print(player_1_sum)
-        print(player_2_sum)
+        print(f'{second_player_name} win with result {player_2_sum} : {player_1_sum}')
+        # print(player_1_sum)
+        # print(player_2_sum)
+
 
 main()
