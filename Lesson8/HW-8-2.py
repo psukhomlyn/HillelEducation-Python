e17897909c1dae4b3402eb307e28a78f32506fe9
psukhomlyn <petrsukhomlin@gmail.com>
2021-04-08 11:57:32 +0300

    commit HT

--- Lesson8/HW-8-2.py
+++ Lesson8/HW-8-2.py
@@ -34,29 +34,36 @@ def main():
             player_1_value = first_player()
             player_2_value = second_player()
             if player_1_value == player_2_value:
+                print(f'No winner in {round_num} round. Result {player_1_value}: {player_2_value}')
                 continue
             player_1_sum += player_1_value
             player_2_sum += player_2_value
 
             print(f'{first_player_name} result in {round_num} round is {player_1_value}')
             print(f'{second_player_name} result in {round_num} round is {player_2_value}')
+            if player_1_value > player_2_value:
+                print(f'{first_player_name} won {round_num} round with result: {player_1_value} : {player_2_value}')
+            # if player_1_value == player_2_value:
+            #     print(f'No winner in {round_num}. Result {player_1_value}: {player_2_value}')
+            else:
+                print(f'{second_player_name} won {round_num} round with result: {player_2_value} : {player_1_value}')
 
             counter -= 1
             round_num += 1
 
         if player_1_sum > player_2_sum:
-            print(f'{first_player_name} win with result {player_1_sum} : {player_2_sum}')
+            print(f'{first_player_name} win game with total result {player_1_sum} : {player_2_sum}')
             # print(player_1_sum)
             # print(player_2_sum)
             return
 
         if player_1_sum == player_2_sum:
-            print(f'Draw with result {player_1_sum} : {player_2_sum}')
+            print(f'Draw with total result {player_1_sum} : {player_2_sum}')
             # print(player_1_sum)
             # print(player_2_sum)
             return
 
-        print(f'{second_player_name} win with result {player_2_sum} : {player_1_sum}')
+        print(f'{second_player_name} win game with total result {player_2_sum} : {player_1_sum}')
         # print(player_1_sum)
         # print(player_2_sum)
 
